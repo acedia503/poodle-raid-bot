98e91ea1b36daf9b946ea1104ad0dd362939314c
acedia503 <acedia503@gmail.com>
2026-03-21 18:11:29 +0900

    Update party_slot_repository.py

--- repositories/party_slot_repository.py
+++ repositories/party_slot_repository.py
@@ -1,1 +1,33 @@
+from typing import Optional
 
+from domain.party_slot import PartySlot
+from repositories.base_repository import BaseRepository
+
+
+class PartySlotRepository(BaseRepository):
+    def bulk_create(self, slots: list[PartySlot]) -> list[PartySlot]:
+        raise NotImplementedError
+
+    def get_by_session_id(self, session_id: int) -> list[PartySlot]:
+        raise NotImplementedError
+
+    def get_by_position(
+        self,
+        session_id: int,
+        group_no: int,
+        party_no: int,
+        slot_no: int,
+    ) -> Optional[PartySlot]:
+        raise NotImplementedError
+
+    def get_by_id(self, slot_id: int) -> Optional[PartySlot]:
+        raise NotImplementedError
+
+    def update_slot(self, slot_id: int, **fields) -> bool:
+        raise NotImplementedError
+
+    def delete_by_session_id(self, session_id: int) -> int:
+        raise NotImplementedError
+
+    def delete_by_id(self, slot_id: int) -> bool:
+        raise NotImplementedError
