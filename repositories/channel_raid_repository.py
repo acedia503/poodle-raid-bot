bf0227d8a08a573ea089207166d7b7633f8672c2
acedia503 <acedia503@gmail.com>
2026-03-21 18:10:41 +0900

    Update channel_raid_repository.py

--- repositories/channel_raid_repository.py
+++ repositories/channel_raid_repository.py
@@ -1,1 +1,21 @@
+from typing import Optional
 
+from domain.channel_raid import ChannelRaid
+from repositories.base_repository import BaseRepository
+
+
+class ChannelRaidRepository(BaseRepository):
+    def get_by_channel_id(self, channel_id: int) -> Optional[ChannelRaid]:
+        raise NotImplementedError
+
+    def get_active_by_channel_id(self, channel_id: int) -> Optional[ChannelRaid]:
+        raise NotImplementedError
+
+    def upsert(self, channel_raid: ChannelRaid) -> ChannelRaid:
+        raise NotImplementedError
+
+    def delete_by_channel_id(self, channel_id: int) -> bool:
+        raise NotImplementedError
+
+    def deactivate_by_channel_id(self, channel_id: int) -> bool:
+        raise NotImplementedError
