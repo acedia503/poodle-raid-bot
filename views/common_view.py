61adcd491cd32cff5a67ba53ad82dd81da05137e
acedia503 <acedia503@gmail.com>
2026-03-21 18:15:39 +0900

    Update common_view.py

--- views/common_view.py
+++ views/common_view.py
@@ -1,1 +1,11 @@
+import discord
 
+
+class CloseView(discord.ui.View):
+    def __init__(self, timeout: int = 180):
+        super().__init__(timeout=timeout)
+
+    @discord.ui.button(label="닫기", style=discord.ButtonStyle.secondary)
+    async def close_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.defer()
+        await interaction.delete_original_response()
