28258dba2c1629a04fab6bd473e747ef8e1beec2
acedia503 <acedia503@gmail.com>
2026-03-22 03:29:31 +0900

    Update raid_application.py

--- domain/raid_application.py
+++ domain/raid_application.py
@@ -1,8 +1,5 @@
 from dataclasses import dataclass
-from datetime import datetime
 from typing import Optional
-
-from domain.enums import ApplicationStatus, SourceType
 
 
 @dataclass
@@ -10,15 +7,14 @@ class RaidApplication:
     id: Optional[int]
     guild_id: int
     channel_id: int
-    raid_name: str
     user_id: int
     user_name: str
     character_name: str
+    race: str
+    server: str
     job: str
     item_level: int
     combat_power: int
-    application_status: ApplicationStatus
-    source_type: SourceType
-    created_by_user_id: int
-    created_at: Optional[datetime] = None
-    updated_at: Optional[datetime] = None
+    raid_name: str
+    created_at: Optional[str] = None
+    updated_at: Optional[str] = None
