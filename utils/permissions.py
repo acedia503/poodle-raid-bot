32cae4d656949cc160c72378105891cccedc097c
acedia503 <acedia503@gmail.com>
2026-03-21 18:15:26 +0900

    Update permissions.py

--- utils/permissions.py
+++ utils/permissions.py
@@ -1,1 +1,9 @@
+def is_admin(interaction) -> bool:
+    if interaction.guild is None:
+        return False
+    perms = interaction.user.guild_permissions
+    return perms.administrator
 
+
+def ensure_guild_only(interaction) -> bool:
+    return interaction.guild is not None
