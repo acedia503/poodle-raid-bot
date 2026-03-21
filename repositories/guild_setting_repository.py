fec495889adb6f4bdc1b29ffacbb167ea9bbbe52
acedia503 <acedia503@gmail.com>
2026-03-21 18:10:29 +0900

    Update guild_setting_repository.py

--- repositories/guild_setting_repository.py
+++ repositories/guild_setting_repository.py
@@ -1,1 +1,23 @@
+from typing import Optional
 
+from domain.guild_setting import GuildSetting
+from repositories.base_repository import BaseRepository
+
+
+class GuildSettingRepository(BaseRepository):
+    def get_by_guild_id(self, guild_id: int) -> Optional[GuildSetting]:
+        # TODO: DB 조회
+        raise NotImplementedError
+
+    def upsert(
+        self,
+        guild_id: int,
+        default_race: str | None,
+        default_server: str | None,
+    ) -> GuildSetting:
+        # TODO: insert or update
+        raise NotImplementedError
+
+    def delete_by_guild_id(self, guild_id: int) -> bool:
+        # TODO: DB 삭제
+        raise NotImplementedError
