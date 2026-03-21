ce5cfbc258267b8c24949b87a366ad3e1514aee5
acedia503 <acedia503@gmail.com>
2026-03-21 18:33:56 +0900

    Update permissions.py

--- utils/permissions.py
+++ utils/permissions.py
@@ -1,9 +1,8 @@
+def ensure_guild_only(interaction) -> bool:
+    return interaction.guild is not None
+
+
 def is_admin(interaction) -> bool:
     if interaction.guild is None:
         return False
-    perms = interaction.user.guild_permissions
-    return perms.administrator
-
-
-def ensure_guild_only(interaction) -> bool:
-    return interaction.guild is not None
+    return interaction.user.guild_permissions.administrator
