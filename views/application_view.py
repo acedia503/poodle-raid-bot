3d5822090aee1427dab45d2e91af440524918c65
acedia503 <acedia503@gmail.com>
2026-03-22 03:55:45 +0900

    Update application_view.py

--- views/application_view.py
+++ views/application_view.py
@@ -1,34 +1,37 @@
 import discord
+
 from utils.constants import RACE_SERVERS
 
 
 class RaceView(discord.ui.View):
-    def __init__(self, callback):
-        super().__init__()
-        self.callback_func = callback
+    def __init__(self, callback_func):
+        super().__init__(timeout=180)
+        self.callback_func = callback_func
 
-    @discord.ui.button(label="천족")
-    async def elyos(self, interaction, button):
+    @discord.ui.button(label="천족", style=discord.ButtonStyle.primary)
+    async def elyos_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.callback_func(interaction, "천족")
 
-    @discord.ui.button(label="마족")
-    async def asmo(self, interaction, button):
+    @discord.ui.button(label="마족", style=discord.ButtonStyle.danger)
+    async def asmodian_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.callback_func(interaction, "마족")
 
 
 class ServerSelect(discord.ui.Select):
-    def __init__(self, race, callback):
+    def __init__(self, race, callback_func):
         options = [
-            discord.SelectOption(label=s["name"], value=s["name"])
-            for s in RACE_SERVERS[race]
+            discord.SelectOption(label=server["name"], value=server["name"])
+            for server in RACE_SERVERS[race]
         ]
+
         super().__init__(
             placeholder="서버를 선택하세요",
             min_values=1,
             max_values=1,
             options=options,
         )
-        self.callback_func = callback
+
+        self.callback_func = callback_func
         self.race = race
 
     async def callback(self, interaction: discord.Interaction):
@@ -36,6 +39,6 @@ class ServerSelect(discord.ui.Select):
 
 
 class ServerView(discord.ui.View):
-    def __init__(self, race, callback):
-        super().__init__()
-        self.add_item(ServerSelect(race, callback))
+    def __init__(self, race, callback_func):
+        super().__init__(timeout=180)
+        self.add_item(ServerSelect(race, callback_func))
