4cc8b1f173a80208406367f4f7342fd0aae9f50a
acedia503 <acedia503@gmail.com>
2026-03-21 18:09:39 +0900

    Update party_build_session.py

--- domain/party_build_session.py
+++ domain/party_build_session.py
@@ -1,1 +1,17 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
 
+from domain.enums import SessionStatus
+
+
+@dataclass
+class PartyBuildSession:
+    id: Optional[int]
+    guild_id: int
+    channel_id: int
+    raid_name: str
+    session_status: SessionStatus
+    created_by_user_id: int
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
