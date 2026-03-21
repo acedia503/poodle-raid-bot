3f321d255bb42e6e43c6467014a1eac28e7d2b28
acedia503 <acedia503@gmail.com>
2026-03-21 18:08:53 +0900

    Update guild_setting.py

--- domain/guild_setting.py
+++ domain/guild_setting.py
@@ -1,1 +1,13 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
 
+
+@dataclass
+class GuildSetting:
+    id: Optional[int]
+    guild_id: int
+    default_race: Optional[str]
+    default_server: Optional[str]
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
