fcc84d17114dddfbe989433ab4f5ec4b19673a5f
acedia503 <acedia503@gmail.com>
2026-03-21 23:36:13 +0900

    Update setting_view.py

--- views/setting_view.py
+++ views/setting_view.py
@@ -1,32 +1,135 @@
 import discord
 
-from services.message_service import MessageService
-from services.setting_service import SettingService
+from utils.constants import RACE_SERVERS
+
+
+class SettingInitRaceView(discord.ui.View):
+    def __init__(self, guild_id, setting_service, message_service):
+        super().__init__(timeout=180)
+        self.guild_id = guild_id
+        self.setting_service = setting_service
+        self.message_service = message_service
+
+    @discord.ui.button(label="천족", style=discord.ButtonStyle.primary)
+    async def elyos_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        view = SettingServerSelectView(
+            guild_id=self.guild_id,
+            selected_race="천족",
+            setting_service=self.setting_service,
+            message_service=self.message_service,
+        )
+        await interaction.response.edit_message(
+            content="종족: **천족**\n서버를 선택하세요.",
+            view=view,
+            embed=None,
+        )
+
+    @discord.ui.button(label="마족", style=discord.ButtonStyle.danger)
+    async def asmodian_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        view = SettingServerSelectView(
+            guild_id=self.guild_id,
+            selected_race="마족",
+            setting_service=self.setting_service,
+            message_service=self.message_service,
+        )
+        await interaction.response.edit_message(
+            content="종족: **마족**\n서버를 선택하세요.",
+            view=view,
+            embed=None,
+        )
+
+
+class SettingServerSelect(discord.ui.Select):
+    def __init__(self, guild_id, selected_race, setting_service, message_service):
+        self.guild_id = guild_id
+        self.selected_race = selected_race
+        self.setting_service = setting_service
+        self.message_service = message_service
+
+        servers = RACE_SERVERS.get(selected_race, [])
+        options = [
+            discord.SelectOption(label=server["name"], value=server["name"])
+            for server in servers
+        ]
+
+        super().__init__(
+            placeholder="서버를 선택하세요",
+            min_values=1,
+            max_values=1,
+            options=options,
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        selected_server = self.values[0]
+
+        setting = self.setting_service.save_guild_setting(
+            guild_id=self.guild_id,
+            default_race=self.selected_race,
+            default_server=selected_server,
+        )
+
+        embed = self.message_service.build_guild_setting_embed(setting)
+        view = SettingMainView(
+            guild_id=self.guild_id,
+            setting_service=self.setting_service,
+            message_service=self.message_service,
+        )
+
+        await interaction.response.edit_message(
+            content="기본 설정이 저장되었습니다.",
+            embed=embed,
+            view=view,
+        )
+
+
+class SettingServerSelectView(discord.ui.View):
+    def __init__(self, guild_id, selected_race, setting_service, message_service):
+        super().__init__(timeout=180)
+        self.add_item(
+            SettingServerSelect(
+                guild_id=guild_id,
+                selected_race=selected_race,
+                setting_service=setting_service,
+                message_service=message_service,
+            )
+        )
 
 
 class SettingMainView(discord.ui.View):
-    def __init__(
-        self,
-        setting_service: SettingService,
-        message_service: MessageService,
-        guild_id: int,
-    ):
+    def __init__(self, guild_id, setting_service, message_service):
         super().__init__(timeout=180)
+        self.guild_id = guild_id
         self.setting_service = setting_service
         self.message_service = message_service
-        self.guild_id = guild_id
-
-    @discord.ui.button(label="설정", style=discord.ButtonStyle.primary)
-    async def create_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        # TODO: 종족/서버 선택 UI로 연결
-        await interaction.response.send_message("설정 UI는 TODO", ephemeral=True)
 
     @discord.ui.button(label="수정", style=discord.ButtonStyle.primary)
     async def update_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message("수정 UI는 TODO", ephemeral=True)
+        view = SettingInitRaceView(
+            guild_id=self.guild_id,
+            setting_service=self.setting_service,
+            message_service=self.message_service,
+        )
+        await interaction.response.edit_message(
+            content="종족을 다시 선택하세요.",
+            embed=None,
+            view=view,
+        )
 
     @discord.ui.button(label="삭제", style=discord.ButtonStyle.danger)
     async def delete_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         ok = self.setting_service.delete_guild_setting(self.guild_id)
-        msg = "기본 설정이 삭제되었습니다." if ok else "삭제할 기본 설정이 없습니다."
-        await interaction.response.send_message(msg, ephemeral=True)
+        msg = "기본 설정이 삭제되었습니다." if ok else "삭제할 설정이 없습니다."
+
+        await interaction.response.edit_message(
+            content=msg,
+            embed=None,
+            view=None,
+        )
+
+    @discord.ui.button(label="닫기", style=discord.ButtonStyle.secondary)
+    async def close_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.edit_message(
+            content="설정 창을 닫았습니다.",
+            embed=None,
+            view=None,
+        )
