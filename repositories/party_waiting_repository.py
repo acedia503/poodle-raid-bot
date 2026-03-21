0d9f98db70c63312dfb2e4fbfa07a0b820ff7fe3
acedia503 <acedia503@gmail.com>
2026-03-21 18:11:44 +0900

    Update party_waiting_repository.py

--- repositories/party_waiting_repository.py
+++ repositories/party_waiting_repository.py
@@ -1,1 +1,27 @@
+from typing import Optional
 
+from domain.party_waiting_member import PartyWaitingMember
+from repositories.base_repository import BaseRepository
+
+
+class PartyWaitingRepository(BaseRepository):
+    def bulk_create(self, waiting_members: list[PartyWaitingMember]) -> list[PartyWaitingMember]:
+        raise NotImplementedError
+
+    def create(self, waiting_member: PartyWaitingMember) -> PartyWaitingMember:
+        raise NotImplementedError
+
+    def get_by_session_id(self, session_id: int) -> list[PartyWaitingMember]:
+        raise NotImplementedError
+
+    def get_by_session_and_status(self, session_id: int, status: str) -> list[PartyWaitingMember]:
+        raise NotImplementedError
+
+    def get_by_id(self, waiting_id: int) -> Optional[PartyWaitingMember]:
+        raise NotImplementedError
+
+    def delete_by_session_id(self, session_id: int) -> int:
+        raise NotImplementedError
+
+    def delete_by_id(self, waiting_id: int) -> bool:
+        raise NotImplementedError
