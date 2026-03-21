fc89d9a23be270079fd810198c3184c8986c1edd
acedia503 <acedia503@gmail.com>
2026-03-21 18:11:02 +0900

    Update raid_rule_repository.py

--- repositories/raid_rule_repository.py
+++ repositories/raid_rule_repository.py
@@ -1,1 +1,15 @@
+from typing import Optional
 
+from domain.raid_rule import RaidRule
+from repositories.base_repository import BaseRepository
+
+
+class RaidRuleRepository(BaseRepository):
+    def get_by_guild_and_raid(self, guild_id: int, raid_name: str) -> Optional[RaidRule]:
+        raise NotImplementedError
+
+    def upsert(self, rule: RaidRule) -> RaidRule:
+        raise NotImplementedError
+
+    def delete_by_guild_and_raid(self, guild_id: int, raid_name: str) -> bool:
+        raise NotImplementedError
