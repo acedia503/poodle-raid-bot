6c3307bbebf605ed6de2ceff599505bcf194ca1b
acedia503 <acedia503@gmail.com>
2026-03-22 18:15:04 +0900

    Update message_service.py

--- services/message_service.py
+++ services/message_service.py
@@ -58,7 +58,7 @@ class MessageService:
         if result_type == "updated":
             title = f"{raid_name} 신청 정보가 조회되었습니다."
         else:
-            title = f"✅{raid_name} 신청 완료"
+            title = f"✅ {raid_name} 신청 완료"
     
         lines = [
             f"**캐릭터명** : {info['character_name']}",
