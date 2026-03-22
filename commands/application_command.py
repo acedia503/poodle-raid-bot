54f62323026abb4b55f39095cfbd9953dfa8f450
acedia503 <acedia503@gmail.com>
2026-03-22 13:11:14 +0900

    Update application_command.py

--- commands/application_command.py
+++ commands/application_command.py
@@ -12,7 +12,7 @@ class ApplicationCommand(commands.Cog):
         self.message_service = message_service
         self.setting_service = setting_service
 
-    @app_commands.command(name="신청", description="레이드 신청 및 확인")
+    @app_commands.command(name="신청", description="레이드 신청 또는 신청 내역 확인")
     @app_commands.rename(character_name="캐릭터명")
     @app_commands.describe(character_name="신청 또는 조회할 캐릭터명")
     async def apply(self, interaction: discord.Interaction, character_name: str):
@@ -48,7 +48,7 @@ class ApplicationCommand(commands.Cog):
 
     async def _process(self, interaction, character_name, race, server, show_identity: bool):
         await interaction.response.defer(ephemeral=True)
-    
+
         result = self.service.process(
             guild_id=interaction.guild.id,
             channel_id=interaction.channel.id,
@@ -58,7 +58,7 @@ class ApplicationCommand(commands.Cog):
             race=race,
             server=server,
         )
-    
+
         if result["action"] == "created":
             embed = self.message_service.build_application_result_embed(
                 result["raid_name"],
@@ -71,7 +71,7 @@ class ApplicationCommand(commands.Cog):
                 embed=embed,
                 view=None,
             )
-    
+
         elif result["action"] == "show_current":
             embed = self.message_service.build_application_result_embed(
                 result["raid_name"],
@@ -84,7 +84,7 @@ class ApplicationCommand(commands.Cog):
                 embed=embed,
                 view=None,
             )
-    
+
         elif result["action"] == "show_all":
             embed = self.message_service.build_application_all_embed(
                 result["info"],
@@ -96,11 +96,10 @@ class ApplicationCommand(commands.Cog):
                 embed=embed,
                 view=None,
             )
-    
+
         else:
             await interaction.edit_original_response(
                 content=result["message"],
                 embed=None,
                 view=None,
             )
-        )
