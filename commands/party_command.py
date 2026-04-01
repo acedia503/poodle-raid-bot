d7d3f8a412c773118df96692204026f114f368cb
acedia503 <acedia503@gmail.com>
2026-04-01 12:31:21 +0900

    Update party_command.py

--- commands/party_command.py
+++ commands/party_command.py
@@ -1,11 +1,13 @@
+# commands/party_command.py
+
 import discord
 from discord import app_commands
 from discord.ext import commands
 
 from views.party_view import (
     PartyBuildHomeView,
-    build_party_result_embed,
     build_empty_result_embed,
+    build_first_status_embed,
 )
 
 
@@ -18,6 +20,7 @@ class PartyCommand(commands.Cog):
         party_builder_service,
         party_manage_service,
         party_modify_service,
+        setting_service,
     ):
         self.bot = bot
         self.raid_service = raid_service
@@ -25,6 +28,30 @@ class PartyCommand(commands.Cog):
         self.party_builder_service = party_builder_service
         self.party_manage_service = party_manage_service
         self.party_modify_service = party_modify_service
+        self.setting_service = setting_service
+
+    def _get_show_race_server(self, guild_id: int) -> bool:
+        """
+        기본 종족/서버 설정이 있으면 False
+        없으면 True
+        """
+        try:
+            setting = self.setting_service.get_guild_setting(guild_id)
+        except Exception:
+            return True
+
+        if setting is None:
+            return True
+
+        if isinstance(setting, dict):
+            default_race = setting.get("default_race")
+            default_server = setting.get("default_server")
+        else:
+            default_race = getattr(setting, "default_race", None)
+            default_server = getattr(setting, "default_server", None)
+
+        has_default_race_server = bool(default_race and default_server)
+        return not has_default_race_server
 
     @app_commands.command(name="공대", description="공대 생성 및 관리")
     async def party(self, interaction: discord.Interaction):
@@ -57,8 +84,13 @@ class PartyCommand(commands.Cog):
             channel_id=channel.id,
         )
 
+        show_race_server = self._get_show_race_server(guild.id)
+
         if result:
-            embed = build_party_result_embed(result)
+            embed = build_first_status_embed(
+                result,
+                show_race_server=show_race_server,
+            )
         else:
             embed = build_empty_result_embed(raid.raid_name)
 
@@ -68,6 +100,7 @@ class PartyCommand(commands.Cog):
             party_builder_service=self.party_builder_service,
             party_manage_service=self.party_manage_service,
             party_modify_service=self.party_modify_service,
+            show_race_server=show_race_server,
         )
 
         await interaction.response.send_message(
