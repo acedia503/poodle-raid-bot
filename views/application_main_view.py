816d64a6caffbfc3c235e52aba7d031774229179
acedia503 <acedia503@gmail.com>
2026-04-27 23:35:05 +0900

    Add cancel button and refactor button methods

--- views/application_main_view.py
+++ views/application_main_view.py
@@ -25,47 +25,27 @@ class ApplicationMainView(discord.ui.View):
         self,
         apply_callback,
         my_applications_callback,
+        cancel_callback,
         status_callback,
-        close_message: str = "신청 창을 닫았습니다.",
     ):
         super().__init__(timeout=180)
         self.apply_callback = apply_callback
         self.my_applications_callback = my_applications_callback
+        self.cancel_callback = cancel_callback
         self.status_callback = status_callback
-        self.close_message = close_message
 
     @discord.ui.button(label="신청하기", style=discord.ButtonStyle.primary)
-    async def apply_button(
-        self,
-        interaction: discord.Interaction,
-        button: discord.ui.Button,
-    ):
+    async def apply_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.apply_callback(interaction)
 
     @discord.ui.button(label="내 신청 확인", style=discord.ButtonStyle.secondary)
-    async def my_applications_button(
-        self,
-        interaction: discord.Interaction,
-        button: discord.ui.Button,
-    ):
+    async def my_applications_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self.my_applications_callback(interaction)
 
-    @discord.ui.button(label="신청 현황", style=discord.ButtonStyle.secondary)
-    async def status_button(
-        self,
-        interaction: discord.Interaction,
-        button: discord.ui.Button,
-    ):
-        await self.status_callback(interaction)
+    @discord.ui.button(label="신청 취소", style=discord.ButtonStyle.danger)
+    async def cancel_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self.cancel_callback(interaction)
 
-    ##@discord.ui.button(label="닫기", style=discord.ButtonStyle.secondary)
-    ##async def close_button(
-    ##    self,
-    ##    interaction: discord.Interaction,
-    ##    button: discord.ui.Button,
-    ##):
-    ##    await interaction.response.edit_message(
-    ##        content=self.close_message,
-    ##        embed=None,
-    ##        view=None,
-    ##    )
+    @discord.ui.button(label="신청 현황", style=discord.ButtonStyle.secondary)
+    async def status_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self.status_callback(interaction)
