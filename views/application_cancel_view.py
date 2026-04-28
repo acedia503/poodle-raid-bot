56ac51a24b41fc30f39eeb3cf64703879dba3d6c
acedia503 <acedia503@gmail.com>
2026-04-28 18:39:05 +0900

    Update application_cancel_view.py

--- views/application_cancel_view.py
+++ views/application_cancel_view.py
@@ -26,45 +26,53 @@ class ApplicationCancelButtonSelectView(discord.ui.View):
 
 
 class ApplicationToggleButton(discord.ui.Button):
-    def __init__(self, parent, index: int, application):
-        selected = application.id in parent.selected_ids
+    def __init__(self, view_ref, index: int, application):
+        selected = application.id in view_ref.selected_ids
         super().__init__(
             label=str(index),
             style=discord.ButtonStyle.success if selected else discord.ButtonStyle.secondary,
             row=0 if index <= 5 else 1,
         )
-        self.parent = parent
+        self.view_ref = view_ref
         self.application = application
 
     async def callback(self, interaction: discord.Interaction):
-        if self.application.id in self.parent.selected_ids:
-            self.parent.selected_ids.remove(self.application.id)
+        if self.application.id in self.view_ref.selected_ids:
+            self.view_ref.selected_ids.remove(self.application.id)
         else:
-            self.parent.selected_ids.add(self.application.id)
+            self.view_ref.selected_ids.add(self.application.id)
 
-        await self.parent.refresh_callback(
+        await self.view_ref.refresh_callback(
             interaction,
-            self.parent.applications,
-            self.parent.selected_ids,
+            self.view_ref.applications,
+            self.view_ref.selected_ids,
         )
 
 
 class CancelSelectedButton(discord.ui.Button):
-    def __init__(self, parent):
-        super().__init__(label="선택 취소", style=discord.ButtonStyle.danger, row=2)
-        self.parent = parent
+    def __init__(self, view_ref):
+        super().__init__(
+            label="선택 취소",
+            style=discord.ButtonStyle.danger,
+            row=2,
+        )
+        self.view_ref = view_ref
 
     async def callback(self, interaction: discord.Interaction):
-        await self.parent.cancel_selected_callback(
+        await self.view_ref.cancel_selected_callback(
             interaction,
-            list(self.parent.selected_ids),
+            list(self.view_ref.selected_ids),
         )
 
 
 class BackButton(discord.ui.Button):
-    def __init__(self, parent):
-        super().__init__(label="뒤로 가기", style=discord.ButtonStyle.secondary, row=2)
-        self.parent = parent
+    def __init__(self, view_ref):
+        super().__init__(
+            label="뒤로 가기",
+            style=discord.ButtonStyle.secondary,
+            row=2,
+        )
+        self.view_ref = view_ref
 
     async def callback(self, interaction: discord.Interaction):
-        await self.parent.back_callback(interaction)
+        await self.view_ref.back_callback(interaction)
