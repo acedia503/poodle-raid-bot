a868e14c03b7ae4be4fb22efe21284dacf7bd4b7
acedia503 <acedia503@gmail.com>
2026-06-27 17:58:33 +0900

    Fix import path for constants in party_builder_service

--- services/party_builder_service.py
+++ services/party_builder_service.py
@@ -2,7 +2,7 @@
 
 import asyncio
 from dataclasses import dataclass, field
-from constents import DEFAULT_GROUP_SIZE, DEFAULT_SLOTS_PER_PARTY
+from utils.constants import DEFAULT_GROUP_SIZE, DEFAULT_SLOTS_PER_PARTY
 
 from domain.party_build_session import PartyBuildSession
 from domain.party_waiting_member import PartyWaitingMember
