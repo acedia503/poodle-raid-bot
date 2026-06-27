42ff49a1fb69e7f0e1172619b0105bafcdaaa907
acedia503 <acedia503@gmail.com>
2026-06-27 17:58:42 +0900

    Update party_modify_service.py

--- services/party_modify_service.py
+++ services/party_modify_service.py
@@ -1,7 +1,7 @@
 # services/party_modify_service.py
 
 from domain.party_slot import PartySlot
-from constents import DEFAULT_GROUP_SIZE, DEFAULT_SLOTS_PER_PARTY
+from utils.constants import DEFAULT_GROUP_SIZE, DEFAULT_SLOTS_PER_PARTY
 
 
 class PartyModifyService:
