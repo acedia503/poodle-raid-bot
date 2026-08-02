b77cbb5eb28e87d5882df8e899c233944f4a2518
acedia503 <acedia503@gmail.com>
2026-08-02 14:19:52 +0900

    Add files via upload

--- models/lol_application.py
+++ models/lol_application.py
@@ -1,1 +1,14 @@
+from dataclasses import dataclass
+from datetime import datetime
 
+
+@dataclass
+class LolApplication:
+    id: int | None
+    guild_id: int
+    user_id: int
+    user_name: str
+    riot_id: str
+    normalized_riot_id: str
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
